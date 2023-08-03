25126c767cba8b6b91ca9de487f6bf18e2fcc277
Suka <mateodefalco1@gmail.com>
2023-08-03 11:54:08 -0300

    Score feedback

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
 	const [word, setWord] = useState(
 		() => randomWords[(Math.random() * randomWords.length) | 0]
 	)
+	const [isPlaying, setIsPlaying] = useState(false)
+	const [score, setScore] = useState(0)
 
 	function handleSubmit(event) {
 		event.preventDefault()
@@ -21,11 +23,15 @@ function App() {
 	}
 
 	useEffect(() => {
-		if (time !== 0) {
+		if (time > 0) {
 			const timeout = setTimeout(() => setTime(time - 1), 1000)
 			return () => clearTimeout(timeout)
+		} else if (time === 0 && isPlaying) {
+			// Calcular el puntaje y mostrar el mensaje
+			const calculatedScore = Math.round((characterCount / 5) * 60)
+			setScore(calculatedScore)
 		}
-	}, [time])
+	}, [time, isPlaying, characterCount])
 
 	return (
 		<div>
@@ -51,17 +57,33 @@ function App() {
 					</form>
 				</div>
 			) : (
-				<div className="flex flex-col items-center gap-4">
-					<h1 className="font-bold text-2xl">Words per minute</h1>
-					<h3 className="text-gray-300">Test your typing skills</h3>
-					<button
-						className="bg-purple-500 text-xl p-1 w-40 font-bold rounded-md"
-						onClick={() => {
-							setTime(60)
-						}}
-					>
-						Play
-					</button>
+				<div>
+					{isPlaying ? (
+						<div className="flex flex-col items-center gap-4">
+							<h1 className="font-bold text-2xl">Game Over</h1>
+							<div className="flex flex-row gap-1">
+								<p>Your Score: </p>
+								<p className="font-bold">{score}</p>
+								<p>words per minute</p>
+							</div>
+						</div>
+					) : (
+						<div className="flex flex-col items-center gap-4">
+							<h1 className="font-bold text-2xl">Words per minute</h1>
+							<h3 className="text-gray-300">Test your typing skills</h3>
+							<button
+								className="bg-purple-500 text-xl p-1 w-40 font-bold rounded-md"
+								onClick={() => {
+									setIsPlaying(true)
+									setTime(60)
+									setCharacterCount(0)
+									setScore(0)
+								}}
+							>
+								Play
+							</button>
+						</div>
+					)}
 				</div>
 			)}
 		</div>
